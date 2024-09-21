5e34365469cb92be6a8f7d5b8a511360079cc32f
khushi agrawal <84030040+Khushi002@users.noreply.github.com>
2024-09-21 12:32:10 +0530

    MissingNumber.cpp

--- MissingNumber.cpp
+++ MissingNumber.cpp
@@ -4,7 +4,7 @@ public:
         // take out arrays size
         int n = nums.size();
 
-        // take sum of n natural numbers because it is sure that numbers will start from 0
+        // takke sum of n natural numbers because it is sure that numbers will start from 0
         int expectedSum = (n*(n+1))/2;
         // take ASum = 0  , then whatever you have in array add that to sctual sum
         int ActualSum = 0;
