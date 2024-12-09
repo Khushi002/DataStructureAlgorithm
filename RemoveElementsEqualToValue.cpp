4739ecd5b62ae5c6d711bf3961c71257284aed8b
khushi agrawal <84030040+Khushi002@users.noreply.github.com>
2024-12-09 08:57:33 +0530

    RemoveElementsEqualToValue.cpp

--- RemoveElementsEqualToValue.cpp
+++ RemoveElementsEqualToValue.cpp
@@ -3,7 +3,7 @@ public:
     int removeElement(vector<int>& nums, int val) {
         int count = 0;// to store unique numbers
         for(int i = 0;i<nums.size();i++){
-            if(nums[i] != val){// at any point if ith index value is not equal to the given value then add it into the count one and increase the count
+            if(nums[i] != val){// at anyy point if ith index value is not equal to the given value then add it into the count one and increase the count
                nums[count] = nums[i];
                count++;
             }
